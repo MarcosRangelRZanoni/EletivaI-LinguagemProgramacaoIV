f57820cbfa9a400c437525aa3f10df7745f48eef
MarcosRangelRZanoni <99424764+MarcosRangelRZanoni@users.noreply.github.com>
2022-03-08 19:48:27 -0300

    Add files via upload

--- Lista1/Exercicio2/resultado.php
+++ Lista1/Exercicio2/resultado.php
@@ -8,10 +8,11 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
-    <title>Exercício 1</title>
+    <title>Exercício 2</title>
   </head>
   <body class="container">
     <?php 
+    echo "<h1>Resultado</h1>";
     $quilo = $_POST['quilo'];
     $qtd_quilo = $_POST['qtd_quilos'];
     
